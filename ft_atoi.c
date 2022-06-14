e484e797f6ddd0aea8cc75708a4b7a9064c0b6f6
rigarrid <ricardogarrido115@gmail.com>
2022-06-14 17:17:34 +0200

    upload of ft_atoi.c

---
+++ ft_atoi.c
@@ -0,0 +1,34 @@
+#include <unistd.h>
+
+int	ft_atoi(char *a)
+{
+	int	num;
+	int	con;
+	int	aux[2];
+
+	num = 0;
+	con = 0;
+	aux[0] = 1;
+	aux[1] = 0;
+	while (a[con] == 32 || (a[con] <= 11 && a[con] > 0))
+		con++;
+	while (a[con] == '+' || a[con] == '-')
+	{
+		if (a[con] == '-')
+		{
+			aux[0] *= -1;
+			aux[1]++;
+		}
+		else if (a[con] == '+')
+			aux[1]++;
+		con++;
+	}
+	while (a[con] >= '0' || a[con] <= '9')
+	{
+		num = (a[con] - 48) + (num * 10);
+		con++;
+	}
+	if (aux[1] > 1)
+		return (0);
+	return (num * aux[0]);
+}
